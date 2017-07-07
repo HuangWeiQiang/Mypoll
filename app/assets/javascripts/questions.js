98f7a0663f60c8abc61629fca802df71b6266c43
HuangWeiQiang <huang_weiqiang@126.com>
2017-07-07 16:08:41 +0800

    delete question.js uesless row

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,5 +1,4 @@
 var loadFile = function (event, index) {
-    var output = document.getElementById('blah' + index);
     $('#blah' + index).attr('src', URL.createObjectURL(event.target.files[0]));
 };
 
