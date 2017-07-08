088224572b57b312477ecb45ebf725a0ecdeee8b
HuangWeiQiang <huang_weiqiang@126.com>
2017-07-08 09:06:10 +0800

    add redirect_to new_user_session_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.html {redirect_to new_user_session_path, notice: exception.message}
+    end
+  end
 end
