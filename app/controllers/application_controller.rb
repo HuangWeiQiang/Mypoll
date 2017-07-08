80acc6f7d5b799545abee40bd5ebcd60674d65b9
HuangWeiQiang <huang_weiqiang@126.com>
2017-07-08 10:33:07 +0800

    Add notice of no access and redirect to new_user_session_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
-      format.html {redirect_to new_user_session_path, notice: exception.message}
+      format.html{redirect_to new_user_session_path, notice: exception.message}
     end
   end
 end
